a7cdf005d9ffa3598e85514ba6934f0cc26470f1
athar <athar@DESKTOP-8QSGDEU>
2025-04-10 16:56:24 +0530

    Add project files.

---
+++ MVC12/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC12.Models;
+
+namespace MVC12.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["Location"] = "India";
+            return View();
+        }
+
+        public IActionResult Student()
+        {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "test",
+                Gender = "Male",
+            };
+            ViewBag.CurrentDate = DateTime.Now.ToShortDateString();
+            ViewData["Title"] = "Mr";
+            string? location= Convert.ToString(TempData["Location"]);
+            return View(student);
+        }
+    }
+}
